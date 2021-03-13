5c9a20a5a2da14e13c5af422d80923702da16d88
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-03-13 17:11:56 +0900

    reactjs_setup_jsx-p1
    reactjs를 설치하고, VSC로 불러와서 component사용해서 jsx 만들어보기 react는 html필요없이 js에 코드 작성해주고 import해주면 바로 브라우저에 나타남 그래서 빠름

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Potato from "./Potato";
+
+function App() {
+  return(
+     <div>
+        <h1>Hello!!!!!!!!!!!!Please!!!!!!!!!!!!!!</h1>
+        <Potato />
+     </div>
+    );
+      
+}
+
+export default App;
