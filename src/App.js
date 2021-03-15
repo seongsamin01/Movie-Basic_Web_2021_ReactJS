42506534a7735830bf1bf7401a3fc74729e48a53
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-03-15 11:39:39 +0900

    map 사용해서 각 음식별 사진넣기
    map은 뭉쳐있는 정보를 각 개별로 호출하기 때문에 덩어리 정보를 받고도 각각의 정보에 개별 이미지 또는 이모티콘 등을 부여할 수 있음

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
 import React from "react";
-import Potato from "./Potato";
+
+function Food({ name, picture }) {
+   return (
+      <div>
+         <h2>I like {name}</h2>
+         <img src={picture} />
+      </div>
+      );
+}
+
+const foodILike = [
+   {
+      name: "파스타",
+      image: "https://recipe1.ezmember.co.kr/cache/recipe/2019/04/01/f8b3042c80a214dd7cc60fa2027cdc9d1.jpg"
+   },
+   {
+      name: "낙곱새",
+      image: "https://t1.daumcdn.net/cfile/tistory/9988FE415E3C3DB021"
+   },
+   {
+      name: "갈비",
+      image: "https://s3-ap-northeast-1.amazonaws.com/dcreviewsresized/20200609084507_photo1_887ca96cced2.jpg"
+   },
+   {
+      name: "스시",
+      image: "https://s3-ap-northeast-1.amazonaws.com/dcreviewsresized/20181209122546_photo1_77513f8dd560.jpg"
+   },
+   {
+      name: "삼겹살",
+      image: "http://ojsfile.ohmynews.com/STD_IMG_FILE/2017/0719/IE002190922_STD.JPG"
+   }
+];
 
 function App() {
-  return(
-     <div>
-        <h1>Hello!!!!!!!!!!!!Please!!!!!!!!!!!!!!</h1>
-        <Potato />
-     </div>
-    );
-      
-}
+   return(
+      <div>
+         {foodILike.map(dish => (
+            <Food name={dish.name} picture={dish.image} />
+         ))}
+         
+      </div>
+     );
+       
+ }
 
 export default App;
--- src/Potato.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function Potato() {
-    return <h3>I love you</h3>;
-
-}
-
-export default Potato;
