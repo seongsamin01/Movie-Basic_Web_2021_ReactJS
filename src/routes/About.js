a942def9ce427cd3fb3d89b8d17a0d1dcac849cc
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-03-16 18:37:11 +0900

    route redirecting
     if (location.state) {
                return <span>{location.state.title}</span>;
            } else {
                return null;
    location.state가 값을 가지면 클릭한 영화의 제목을 보여주고 그렇지 않다면 초기페이지로 돌아가라?
    
    css 공부 좀 집중적으로 더 해야겠다..
    css가 지금은 엉망인데 잘만 공부하면 예쁜 사이트 만들 수 있을 것 같다.

--- src/routes/About.js
+++ src/routes/About.js
@@ -5,9 +5,9 @@ function About(props) {
     return (
       <div className="about__container">
         <span>
-            "내가 성공할 수 있는 이유는, 내가 성공할 수 있다고 믿기 때문이다.""
+            "나는 성공할 것이다. 그 이유는 내가 성공할 수 있다고 믿기 때문이다.""
         </span>
-        <span>By 성공한 사업가 민정</span>
+        <span>--By Seongsamin--</span>
         </div>
     );
 }
